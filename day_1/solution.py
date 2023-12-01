673f2383c10f663d017e8553aba2957d9f84ef79
Laurence Cruz <lcruz@squarespace.com>
2023-12-01 10:53:58 -0500

    refactor: parse input file once

--- day_1/solution.py
+++ day_1/solution.py
@@ -1,16 +1,19 @@
-import re;
+import re
 
-def part_one(filename):
+def parse_input_file(filename):
+    with open(filename, 'r') as file:
+        return file.readlines()
+
+def part_one(lines):
     total_sum = 0
 
-    for line in open(filename):
+    for line in lines:
         digits = [ch for ch in line if ch.isdigit()]
         if digits:
             total_sum += int(digits[0] + digits[-1])
     return total_sum
 
-
-def part_two(filename):
+def part_two(lines):
     total_sum = 0
     n = "one two three four five six seven eight nine".split()
     p = "(?=(" + "|".join(n) + "|\\d))"
@@ -20,12 +23,14 @@ def part_two(filename):
             return str(n.index(x) + 1)
         return x
 
-    for line in open(filename):
-        digits = [*map(index, re.findall(p,line))]
+    for line in lines:
+        digits = [*map(index, re.findall(p, line))]
         total_sum += int(digits[0] + digits[-1])
     return total_sum
 
-part_one_result = part_one('in.txt')
-part_two_result = part_two('in.txt')
+file_contents = parse_input_file('in.txt')
+part_one_result = part_one(file_contents)
+part_two_result = part_two(file_contents)
+
 print(part_one_result)
 print(part_two_result)
